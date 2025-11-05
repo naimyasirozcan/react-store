ee12dd7088d066775f81cc974b315a03b8b6185b
naimyasirozcan <naimyasirozcan@gmail.com>
2025-11-05 18:41:54 +0100

    lab solved without styling.

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState(null);
 
+  const { productId } = useParams()
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setProduct(data)
+      })
+
+  }, [productId])
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
@@ -14,10 +28,23 @@ function ProductDetailsPage() {
   // To fetch the product details, set up an effect with the `useEffect` hook:
 
 
-
+  if (!product) {
+    return <h3> Searching...</h3>
+  }
   return (
-    <div className="ProductDetailsPage">
-    {/* Render product details here */}
+    <div >
+      <img src={product.image} alt="" />
+      <h1>{product.title}</h1>
+      <h4 style={{fontWeight: "800"}}>Price:</h4>
+      <h3>{product.price}</h3>
+      <h4 style={{fontWeight: "800"}}>Description:</h4>
+      <h4>{product.description}</h4>
+      <h4 style={{fontWeight: "800"}}>Category:</h4>
+      <h4>{product.category}</h4>
+      <h4 style={{fontWeight: "800"}}>Rating:</h4>
+      <h5>{product.rating.rate}
+        <br />
+        rated by <strong>{product.rating.count} </strong> users</h5>
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function ProductListPage() {
   // The state variable `products` is currently an empty array [], 
@@ -8,10 +8,105 @@ function ProductListPage() {
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
 
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => {
+        return response.json()
+      })
+      .then((productsData) => {
+        setProducts(productsData)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+
+  // --------------------------style----------------------------------
+
+  const productListStyle = {
+    display: "flex",
+    flexDirection:"row",
+    justifyContent: "space-evenly",
+    alignItems:"center",
+    width: "100%",
+    height:"500px"
+  }
+  const productListImageStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems:"center",
+    width: "70%",
+    height:"160px"
+  }
+  const productListTitleStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems:"center",
+    fontSize: "16px"
+  }
+  const productListCategoryStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems:"center",
+    fontSize: "16px"
+  }
+  const productListPriceStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems:"center",
+    fontSize: "16px"
+  }
+  const productListDescriptionStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems:"center",
+    fontSize: "16px"
+  }
+
+
+
+
+
+
+  // --------------------------style----------------------------------
+
 
   return (
-    <div className="ProductListPage">
-      {/* Render list of products here */}
+    <div style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}} className="ProductListPage">
+      {
+        products.map((eachProduct) => {
+          return (
+            <div style={productListStyle}>
+              <Link key={eachProduct.id} to={`/product/details/${eachProduct.id}`}>
+              <div style={productListImageStyle}>
+                <img style={{height:"120px",width:"150px"}} src={eachProduct.image} alt="" />
+              </div>
+              <div style={productListTitleStyle}>
+                <h6>{eachProduct.title}</h6>
+              </div>
+              <div style={productListCategoryStyle}>
+                <h6>{eachProduct.category}</h6>
+
+              </div>
+              <div style={productListPriceStyle}>
+
+                <h6>{eachProduct.price}</h6>
+              </div>
+              <div style={productListDescriptionStyle}>
+                <h6>{eachProduct.description}</h6>
+
+              </div>
+              
+              
+              
+              </Link>
+            </div>
+          )
+        }
+
+        )
+      }
     </div>
   );
 }
